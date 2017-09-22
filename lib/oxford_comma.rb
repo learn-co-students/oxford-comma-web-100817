c03244a0d5292ddc16cd7128e72444541678efa1
Ryan Mascolo <rjmascolo@gmail.com>
2017-09-22 19:01:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  line = ""
+  if array.length === 1
+    line += array[0]
+  elsif array.length === 2
+    line += "#{array[0]} and #{array[1]}"
+  else
+    array.each.with_index do |item, index|
+      line += "#{item}#{array.length - 2 === index ? ", and " : array.length - 1 === index ? "" : ", "}"
+    end
+  end
+  return line
 end
