7df481bc5390dba5e64c2e0b014d359267a30501
flored27 <dflores@fandm.edu>
2017-09-27 13:18:21 -0400

    piece of pastel

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+if array.length == 1
+  return array[0]
+elsif array.length == 2
+  return array.join(" and ")
+elsif array.length == 3
+  lastitem = "and #{array.last}"
+  array.pop
+  array.push(lastitem)
+  return array.join(", ")
+else
+  lastitem = "and #{array.last}"
+  array.pop
+  array.push(lastitem)
+  return array.join(", ")
+end
 end
